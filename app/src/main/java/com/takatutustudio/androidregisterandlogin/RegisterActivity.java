9dff0850875e3c4c9be0eea16e32f28aa166488d
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-07-09 17:27:31 +0700

    Menyelesaikan UI dan Java

--- app/src/main/java/com/takatutustudio/androidregisterandlogin/RegisterActivity.java
+++ app/src/main/java/com/takatutustudio/androidregisterandlogin/RegisterActivity.java
@@ -2,12 +2,74 @@ package com.takatutustudio.androidregisterandlogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText name, email, password, c_password;
+    private Button btn_regist;
+    private ProgressBar loading;
+    private static String URL_REGIST = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        loading     = findViewById(R.id.loading);
+        name        = findViewById(R.id.name);
+        email       = findViewById(R.id.email);
+        password    = findViewById(R.id.password);
+        c_password  = findViewById(R.id.c_password);
+
+
+        btn_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+            }
+        });
     }
+
+    private void Regist(){
+        loading.setVisibility(View.VISIBLE);
+        btn_regist.setVisibility(View.GONE);
+
+        final String name       = this.name.getText().toString().trim();
+        final String email      = this.email.getText().toString().trim();
+        final  String password  = this.password.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                })
+
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return super.getParams();
+            }
+        };
+    }
+
 }
