2cbdb3ddd82a64c23633cae863b94e684c6c5efe
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-07-14 16:00:51 +0700

    Membuat LoginActivity

--- app/src/main/java/com/takatutustudio/androidregisterandlogin/RegisterActivity.java
+++ app/src/main/java/com/takatutustudio/androidregisterandlogin/RegisterActivity.java
@@ -6,38 +6,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText name, email, password, c_password;
-    private Button btn_regist;
-    private ProgressBar loading;
-    private static String URL_REGIST = "";
+    private EditText        name, email, password, c_password;
+    private Button          btn_regist;
+    private ProgressBar     loading;
+    private static String   URL_REGIST = "http://192.168.43.4/android_register_login/register.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+         setContentView(R.layout.activity_register);
 
         loading     = findViewById(R.id.loading);
         name        = findViewById(R.id.name);
         email       = findViewById(R.id.email);
         password    = findViewById(R.id.password);
         c_password  = findViewById(R.id.c_password);
+        btn_regist  = findViewById(R.id.btn_regist);
 
 
         btn_regist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //
+                Regist();
             }
         });
     }
@@ -48,18 +56,36 @@ public class RegisterActivity extends AppCompatActivity {
 
         final String name       = this.name.getText().toString().trim();
         final String email      = this.email.getText().toString().trim();
-        final  String password  = this.password.getText().toString().trim();
+        final String password   = this.password.getText().toString().trim();
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String success = jsonObject.getString("success");
+
+                        if (success.equals("1")) {
+                            Toast.makeText(RegisterActivity.this, "Register Success!", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(RegisterActivity.this, "Register Error! " + e.toString(), Toast.LENGTH_SHORT).show();
+                        loading.setVisibility(View.GONE);
+                        btn_regist.setVisibility(View.VISIBLE);
 
                     }
-                },
+                    }
+
+                    },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RegisterActivity.this, "Register Error!" + error.toString(), Toast.LENGTH_SHORT).show();
+                        loading.setVisibility(View.GONE);
+                        btn_regist.setVisibility(View.VISIBLE);
 
                     }
                 })
@@ -67,9 +93,16 @@ public class RegisterActivity extends AppCompatActivity {
         {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                return super.getParams();
+                Map<String, String> params = new HashMap<>();
+                params.put("name", name);
+                params.put("email", email);
+                params.put("password", password);
+                return params;
             }
         };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
 }
