0fc49af5b2480db8e2a773dfcabebf8b9c8588c9
zerkijulian92 <zerkijuliandri92@gmail.com>
2019-07-14 16:58:06 +0700

    edit java LoginActivity.java

--- app/src/main/java/com/takatutustudio/androidregisterandlogin/LoginActivity.java
+++ app/src/main/java/com/takatutustudio/androidregisterandlogin/LoginActivity.java
@@ -2,12 +2,29 @@ package com.takatutustudio.androidregisterandlogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button btn_login;
+    private TextView link_regist;
+    private ProgressBar loading;
+    private static String URL_LOGIN = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loading = findViewById(R.id.loading);
+        email   = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btn_login = findViewById(R.id.btn_login);
+        link_regist = findViewById(R.id.link_regist);
+
     }
 }
